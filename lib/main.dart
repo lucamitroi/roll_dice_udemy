c1586d44c98046e6b458b32248ff24e23d7ab332
Luca Mitroi <lucamitroi@yahoo.com>
2023-07-20 17:35:51 +0300

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_udemy/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("Hello World!")));
+  const List<Color> colorList = [
+    Color.fromARGB(255, 26, 2, 80),
+    Color.fromARGB(255, 45, 7, 98),
+  ];
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(colors: colorList),
+      ),
+    ),
+  );
 }
